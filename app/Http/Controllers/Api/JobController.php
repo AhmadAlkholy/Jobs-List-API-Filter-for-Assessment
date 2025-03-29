5a791ff962a0ac192ad161660c437573c3c9b2b9
ahmedalkholy49 <ahmedalkholy49@gmail.com>
2025-03-29 10:48:10 +0200

    feat: add JobControlle which returns the api response on 'get jobs' api

---
+++ app/Http/Controllers/Api/JobController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Job;
+use App\Services\FilterBuilderService;
+use App\Http\Resources\JobResource;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class JobController extends Controller
+{
+    public function index(Request $request, FilterBuilderService $filterBuilder)
+    {
+        $query = Job::with(['languages', 'locations', 'categories', 'attributes', 'attributeValues']);
+        
+        try {
+            $filterBuilder->applyFilters($query, $request->input('filter'));
+            $jobs = $query->paginate(10);
+        }
+        catch (\Exception $e) {
+            Log::error("Invalid filters for jobs api | filter query: {$request->input('filter')}");
+            return $this->failed($e->getMessage());
+        }
+
+        return $this->paginate('Jobs fetched successfully.', JobResource::collection($jobs));
+    }
+}
